5531764269b32ded6b12aa3ed680d3be5a4365fc
Matthijs Berends <m.berends@certe.nl>
2018-01-26 11:27:11 +0100

    v1.0.0 - First release

--- R/init.R
+++ R/init.R
@@ -2,8 +2,8 @@
 #'
 #' Compare text files line by line and see their differences immediately. You can use filepaths as input, or start the app and select them in the app (or use drag and drop). The function \code{textcompare_active} can be used in RStudio to use the active source document as input for \code{file1}.
 #' @param file1,file2 Path of files. If not specified, an input box will be shown that supports drag and drop for files.
-#' @param file1_colour,file2_colour HTML-values to display as difference between file1 and file2. Defaults tot GitHub colours.
-#' @param title Title to display
+#' @param file1_backgroundcolor,file1_foregroundcolor,file2_backgroundcolor,file2_foregroundcolor HTML-values to display as difference between file1 and file2. Defaults tot GitHub colours. This can be used to theme the app yourself.
+#' @param title Title to display. This can be used to theme the app yourself.
 #'
 #' @import shiny
 #' @importFrom grDevices rgb
@@ -14,16 +14,15 @@
 #' @export
 textcompare <- function(file1 = NULL,
                         file2 = NULL,
-                        file1_colour = rgb(253, 184, 192, maxColorValue = 255),
-                        file2_colour = rgb(172, 242, 189, maxColorValue = 255),
+                        file1_backgroundcolor = rgb(253, 184, 192, maxColorValue = 255),
+                        file1_foregroundcolor = "black",
+                        file2_backgroundcolor = rgb(172, 242, 189, maxColorValue = 255),
+                        file2_foregroundcolor = "black",
                         title = "Shiny TextCompare") {
 
   # internal helper functions
   filename <- function(file) {
     rev(unlist(strsplit(gsub("/", "\\", file, fixed = TRUE), "\\", fixed = TRUE)))[1]
-  }
-  filelastedited <- function(file) {
-    file.info(file)
   }
   human_filesize <- function(bytes, decimals = 1) {
     # Adapted from:
@@ -39,15 +38,35 @@ textcompare <- function(file1 = NULL,
   escapeHTML <- function(text) {
     htmltools::htmlEscape(text = text, attribute = TRUE)
   }
+  printline <- function(linenumA = "", linenumB = "", linetext = NA, printed_A, printed_B) {
+    if (!is.na(linetext)
+        & !(linenumA != "" & linenumA %in% printed_A)
+        & !(linenumB != "" & linenumB %in% printed_B)) {
+      if (linenumA != "" & linenumB != "") {
+        lineclass <- "same"
+      } else if (linenumA != "") {
+        lineclass <- 'file1'
+        linenumB <- "-"
+      } else if (linenumB != "") {
+        lineclass <- 'file2'
+        linenumA <- "-"
+      }
+      paste0('<div class="line ', lineclass, ' linenumber">', linenumA, '</div>',
+             '<div class="line ', lineclass, ' linenumber">', linenumB, '</div>',
+             '<pre class="line ', lineclass, '">', linetext, '</pre>',
+             '<br>')
+    }
+  }
 
   # process file 1
   if (!is.null(file1)) {
     if (!file.exists(file1)) {
       stop('file1 does not exist')
     }
-    file_input1 <- data.frame(name = filename(file1),
+    file_input1 <- data.frame(id = "X",
+                              name = filename(file1),
                               size = file.size(file1),
-                              type = character(0),
+                              type = character(1),
                               datapath = file1)
     file_input1_form <- p('(File 1 already selected)', class = "file_already_selected")
   } else {
@@ -60,9 +79,10 @@ textcompare <- function(file1 = NULL,
     if (!file.exists(file2)) {
       stop('file2 does not exist')
     }
-    file_input2 <- data.frame(name = filename(file2),
+    file_input2 <- data.frame(id = "X",
+                              name = filename(file2),
                               size = file.size(file2),
-                              type = character(0),
+                              type = character(1),
                               datapath = file2)
     file_input2_form <- p('(File 2 already selected)', class = "file_already_selected")
   } else {
@@ -81,8 +101,10 @@ textcompare <- function(file1 = NULL,
     tags$head(
       tags$style(HTML(
         paste(
-          ".file1 { background-color:", file1_colour, "!important;}",
-          ".file2 { background-color:", file2_colour, "!important;}")
+          ".file1 { color:", file1_foregroundcolor, "!important; background-color:", file1_backgroundcolor, "!important;}",
+          "tbody > tr:first-child > td:first-child { color:", file1_foregroundcolor, "!important; background-color:", file1_backgroundcolor, "!important;}",
+          ".file2 { color:", file2_foregroundcolor, "!important; background-color:", file2_backgroundcolor, "!important;}",
+          "tbody > tr:last-child > td:first-child { color:", file2_foregroundcolor, "!important; background-color:", file2_backgroundcolor, "!important;}")
       ))),
     sidebarLayout(
       if (!is.data.frame(file_input1) | !is.data.frame(file_input2)) {
@@ -98,8 +120,8 @@ textcompare <- function(file1 = NULL,
       },
       mainPanel(
         h3(title),
-        #img(src = 'logo.png'),
         tableOutput("contents"),
+        uiOutput("contents2"),
         hr(),
         uiOutput("comparison"),
         width = main_width
@@ -126,14 +148,16 @@ textcompare <- function(file1 = NULL,
         return(NULL)
       }
       if (!is.null(inFile1)) {
-        df_contents <- data.frame(name = inFile1[1, 'name'],
+        df_contents <- data.frame(id = "X",
+                                  name = inFile1[1, 'name'],
                                   size = inFile1[1, 'size'],
                                   type = mime::guess_type(as.character(inFile1[1, 'datapath'])),
                                   modi = as.character(file.mtime(as.character(inFile1[1, 'datapath']))),
                                   hash = tools::md5sum(as.character(inFile1[1, 'datapath'])))
         if (!is.null(inFile2) & is.data.frame(inFile2)) {
           df_contents <- rbind(df_contents,
-                               data.frame(name = inFile2[1, 'name'],
+                               data.frame(id = "X",
+                                          name = inFile2[1, 'name'],
                                           size = inFile2[1, 'size'],
                                           type = mime::guess_type(as.character(inFile2[1, 'datapath'])),
                                           modi = as.character(file.mtime(as.character(inFile2[1, 'datapath']))),
@@ -141,8 +165,40 @@ textcompare <- function(file1 = NULL,
         }
       }
       df_contents[, 'size'] <- human_filesize(df_contents[, 'size'])
-      colnames(df_contents) <- c('File Name', 'File Size', 'File Type', 'Last modified', 'MD5 Hash')
+      colnames(df_contents) <- c(' ', 'File Name', 'File Size', 'File Type', 'Last modified', 'MD5 Hash')
       df_contents
+    })
+
+    output$contents2 <- renderUI({
+
+      if (is.null(input$file1)) {
+        file1 <- file_input1
+      } else {
+        file1 <- input$file1
+      }
+      if (is.null(input$file2)) {
+        file2 <- file_input2
+      } else {
+        file2 <- input$file2
+      }
+
+      File1 <- as.character(file1$datapath)
+      File2 <- as.character(file2$datapath)
+
+      if (length(File1) == 0 | length(File2) == 0) {
+        return(NULL)
+      }
+
+      timedif <- difftime(file.mtime(File2), file.mtime(File1))
+      div(HTML(paste0("Time difference of <b>",
+          round(abs(timedif)),
+          " ",
+          attributes(timedif)$units,
+          "</b>. Size difference of <b>",
+          human_filesize(abs(file.size(File2) - file.size(File1))),
+          "</b>."
+        )))
+
     })
 
     output$comparison <- renderText({
@@ -179,11 +235,8 @@ textcompare <- function(file1 = NULL,
       # - show file1 in red until there are no more differences
       # - show file 2 in green until there are no more differences
 
-      offset_A <- 0
-      offset_B <- 0
-
-      lines_todo_A <- length(A)
-      lines_todo_B <- length(B)
+      printed_A <- integer(0)
+      printed_B <- integer(0)
 
       linenum_A <- 1
       linenum_B <- 1
@@ -200,30 +253,28 @@ textcompare <- function(file1 = NULL,
         if (is.na(A[linenum_A])) {
           # we're thru with A, print rest of B and break
           for (j in linenum_B:length(B)) {
-            out <- paste0(out,
-                          '<div class="line file2 linenumber linenumber_file1"> </div>',
-                          '<div class="line file2 linenumber linenumber_file2">', j, '</div>',
-                          '<pre class="line file2">', B[j], '</pre>')
+            out <- paste0(out, printline(linenumB = j, linetext = B[j],
+                                         printed_A = printed_A, printed_B = printed_B))
+            printed_B <- c(printed_B, linenum_B)
             add <- add + 1
             break
           }
         } else if (is.na(B[linenum_B])) {
           # we're thru with B, print rest of A and break
           for (j in linenum_A:length(A)) {
-            out <- paste0(out,
-                          '<div class="line file1 linenumber linenumber_file1">', j, '</div>',
-                          '<div class="line file1 linenumber linenumber_file2"> </div>',
-                          '<pre class="line file1">', A[j], '</pre>')
+            out <- paste0(out, printline(linenumA = j, linetext = A[j],
+                                         printed_A = printed_A, printed_B = printed_B))
+            printed_A <- c(printed_A, linenum_A)
             del <- del + 1
             break
           }
 
         } else if (A[linenum_A] == B[linenum_B]) {
           # same, just print
-          out <- paste0(out,
-                        '<div class="line same linenumber linenumber_file1">', linenum_A, '</div>',
-                        '<div class="line same linenumber linenumber_file2">', linenum_B, '</div>',
-                        '<pre class="line same">', A[linenum_A], '</pre>')
+          out <- paste0(out, printline(linenumA = linenum_A, linenumB = linenum_B, linetext = A[linenum_A],
+                                       printed_A = printed_A, printed_B = printed_B))
+          printed_A <- c(printed_A, linenum_A)
+          printed_B <- c(printed_B, linenum_B)
 
         } else {
           # differs, print A until B has a same line
@@ -232,41 +283,41 @@ textcompare <- function(file1 = NULL,
           for (j in linenum_A:length(A)) {
             counter <- counter + 1
             #print(paste('   DIFFERS, counter is now', counter, 'and A is now', linenum_A, 'and B is now', linenum_B))
-            out <- paste0(out,
-                          '<div class="line file1 linenumber linenumber_file1">', j, '</div>',
-                          '<div class="line file1 linenumber linenumber_file2"> </div>',
-                          '<pre class="line file1">', A[j], '</pre>')
+            out <- paste0(out, printline(linenumA = j, linetext = A[j],
+                                         printed_A = printed_A, printed_B = printed_B))
+            printed_A <- c(printed_A, linenum_A)
+
             linenum_A <- linenum_A + 1
             del <- del + 1
-            if (A[linenum_A] %in% B) {
+            if (A[linenum_A + 1] == B[linenum_B + 1]) {
+              offset_B <- 1
+            } else {
+              offset_B <- 0
+            }
+            if (A[linenum_A] %in% B[(linenum_B + offset_B):length(B)]) {
               # next line found in B, so set A to that point and print B until here
-              #linenum_A <- which(A[linenum_A + counter] == B) - 1
               for (k in linenum_B:linenum_A) {
                 # print B until A has a same line
                 counter2 <- counter2 + 1
-                out <- paste0(out,
-                              '<div class="line file2 linenumber linenumber_file1"> </div>',
-                              '<div class="line file2 linenumber linenumber_file2">', k, '</div>',
-                              '<pre class="line file2">', B[k], '</pre>')
+                out <- paste0(out, printline(linenumB = k, linetext = B[k],
+                                             printed_A = printed_A, printed_B = printed_B))
+                printed_B <- c(printed_B, linenum_B)
                 add <- add + 1
-                #linenum_B <- linenum_B + 1
-                if (B[k + counter2] %in% A) {
+                if (B[k + counter2] %in% A[linenum_A:length(A)]) {
                   linenum_B <- linenum_B + counter2
-                  #print(paste("counter2:", counter2))
-                  #linenum_B <- which(B[k + counter2] == A) - 1
+                  # print(paste("counter2:", counter2))
                   break
                 }
               }
               break
             }
           }
-          #linenum_B <- linenum_B + counter - 1
           linenum_A <- linenum_A - 1
           linenum_B <- linenum_B - 1
           # print(paste('   CHANGED, counter is now', counter, 'and A is now', linenum_A, 'and B is now', linenum_B))
         }
 
-        out <- paste0(out, '<br>')
+        #out <- paste0(out, '<br>')
 
         linenum_A <- linenum_A + 1
         linenum_B <- linenum_B + 1
@@ -291,8 +342,10 @@ textcompare <- function(file1 = NULL,
 #' @importFrom utils choose.files installed.packages
 #' @export
 textcompare_active <- function(file2 = NULL,
-                               file1_colour = rgb(253, 184, 192, maxColorValue = 255),
-                               file2_colour = rgb(172, 242, 189, maxColorValue = 255),
+                               file1_backgroundcolor = rgb(253, 184, 192, maxColorValue = 255),
+                               file1_foregroundcolor = "black",
+                               file2_backgroundcolor = rgb(172, 242, 189, maxColorValue = 255),
+                               file2_foregroundcolor = "black",
                                title = "Shiny TextCompare") {
   if (!"rstudioapi" %in% utils::installed.packages()) {
     stop("this function only works in RStudio")
@@ -313,8 +366,10 @@ textcompare_active <- function(file2 = NULL,
 
   textcompare(file1 = file1,
               file2 = file2,
-              file1_colour = file1_colour,
-              file2_colour = file2_colour,
+              file1_backgroundcolor = file1_backgroundcolor,
+              file1_foregroundcolor = file1_foregroundcolor,
+              file2_backgroundcolor = file2_backgroundcolor,
+              file2_foregroundcolor = file2_foregroundcolor,
               title = title)
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# `shiny-textcompare`
+# `shinyTextCompare`
 Compare text files with each other, just like the revision pages of GitHub.
 
-Also supports the Addin system of RStudio.
+![window2](inst/rstudio/window2.png)
+
+
+## Using RStudio Addins
+In RStudio, you can press the button **Addins** to open this Shiny app:
+
+![addin](inst/rstudio/addin.png)
+
+The same window as above will appear. With the function `Compare active file with file...` the file that is opened in the Source Editor will be used as the first file. A popup screen will appear to select a second file. This is the fastest way to compare the current file with e.g. older versions.
+
+
+## Using the Console
+```r
+library(shinyTextCompare)
+textcompare()
+```
+Now a new window appears. Just drag your files to the app and they will be compared line by line.
+
+![window1](inst/rstudio/window1.png)
+
+
+```r
+# Without upload, defining both files directly:
+textcompare("file1.txt", "file2.txt")
+```
+
+### Theming
+
+```r
+# Set specific colours to the app, must be CSS valid:
+textcompare(file1_backgroundcolor = "black",
+            file1_foregroundcolor = "white",
+            file2_backgroundcolor = "yellow",
+            file2_foregroundcolor = "red",
+            title = "My Awesome App Title")
+```
+![window1](inst/rstudio/window3.png)
